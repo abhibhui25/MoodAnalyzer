e5d6908e0fecf9db18c0b436bc37840eeeff004d
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-11 20:24:02 +0530

    tested for sad mood

--- MoodAnalyzer/src/main/java/com/cg/moodanalyzer/MoodAnalyzer.java
+++ MoodAnalyzer/src/main/java/com/cg/moodanalyzer/MoodAnalyzer.java
@@ -1,15 +1,9 @@
 package com.cg.moodanalyzer;
-import java.util.Scanner;
+
 public class MoodAnalyzer {
-        String message=null;
-
-
-        public String analyzeMood(String message) {
-            if(message.equalsIgnoreCase("happy"))
-                return "happy";
-            else if(message.equalsIgnoreCase("sad"))
-                return "sad";
-        }
+        public String analyzeMood(String message){
+            return "sad";
 
     }
+}
 
--- MoodAnalyzer/src/test/java/com/cg/moodanalyzer/MoodAnalyzerTest.java
+++ MoodAnalyzer/src/test/java/com/cg/moodanalyzer/MoodAnalyzerTest.java
@@ -1,4 +1,13 @@
 package com.cg.moodanalyzer;
 
+import org.junit.Test;
+import org.junit.Assert;
 public class MoodAnalyzerTest {
+
+    @Test
+    public void givenMessage_WhenSadShouldReturnSad() {
+        MoodAnalyzer m=new MoodAnalyzer();
+        String mood=m.analyzeMood("I am in sad mood");
+        Assert.assertEquals("sad",mood);
+    }
 }
